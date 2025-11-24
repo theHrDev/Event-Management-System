d135f23f69e59aa707949fb56aabaa396a79f422
theHrDev <hamzatrukayat2822@gmail.com>
2025-11-24 08:28:12 +0100

    Added the event management function

--- event.py
+++ event.py
@@ -24,8 +24,8 @@
 
 events = {
     "Tech Summit":["Taye", "Kenny"],
-    "Sports Festival": [],
-    "Music Fiesta": []
+    "Sports Festival": ["Lilly","Killy"],
+    "Music Fiesta": ["Poppoal","koll","kktl"]
 }
 
 # print(events["Tech Summit"][0])
@@ -63,14 +63,28 @@ def get_participants(event):
     return events[event]
 
 def find_events_with_lowest_participants():
-    min = 0
-    for event in events:
-        total_num = len(event)
-        if min > total_num:
-            min = total_num
-            
-    return f"The event with the lowest num is {event}"
-        
+    for names in events:
+        return names
+
+
+def event_management(menu):
+    if menu == "1":
+        event = input("Enter the event name: ")
+        name = input("Enter user's name: ")
+        register_user(event=event,name=name)
+    elif menu == "2":
+        event = input("Enter the event name: ")
+        name = input("Enter user's name: ")
+        unregistered_user(event,name)
+    elif menu == "3":
+        event = input("Enter the event name: ")
+        get_participants(event)
+    else:
+        exit()
+
+menu = input("Choose a menu: \n 1. Register user \n 2. Unregister user \n 3. View participants \n 4. Show event with fewest participants \n 5. Show event with most participants \n 6. Exit: ")
+event_management(menu=menu)
+    
 
 
 
