3e86139c87ed09fdcbf214a20a873ede42a11967
theHrDev <hamzatrukayat2822@gmail.com>
2026-01-10 21:04:29 +0100

    events class

--- event_oop.py
+++ event_oop.py
@@ -5,3 +5,13 @@ class User:
 class Event:
     def __init__(self,event):
         self.event = event
+        
+
+class Events:
+    def __init__(self,id,event,users):
+        self.id = id
+        self.event = event
+        self.users = users
+        
+    def register_user(self,u):
+        pass
