651a142d1fc73ad77505b3839c31ec313051c128
theHrDev <hamzatrukayat2822@gmail.com>
2026-01-11 15:25:36 +0100

    Added the create_event method

--- event_oop.py
+++ event_oop.py
@@ -1,17 +1,34 @@
+import random
 class User:
     def __init__(self,name):
         self.name = name
 
 class Event:
-    def __init__(self,event):
+    def __init__(self,id,event):
+        self.id = id
         self.event = event
+        self.users = []
+        
         
 
 class Events:
-    def __init__(self,id,event,users):
-        self.id = id
-        self.event = event
-        self.users = users
+    def __init__(self):
+        self.events = {}
         
-    def register_user(self,u):
+    def generate_id():
+        random_id = random.randint(10*5,10*6-1)   
+        event_id = f"event{random_id}"
+        return event_id; 
+    
+    def create_event(self,event_name):
+        event_id = self.generate_id()
+        new_event = Event(id=event_id,event=event_name)
+        self.events[event_id] = new_event
+        return f"event created successfully"  
+          
+    def edit_event(self):
+        pass
+    def view_event(self):
+        pass
+    def delete_event(self):
         pass
