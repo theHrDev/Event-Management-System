ed16f0b16739fbfa675cfb4a06f133b7b8e453c0
theHrDev <hamzatrukayat2822@gmail.com>
2026-01-12 17:06:47 +0100

    Added the delete event, view all events method

--- event_oop.py
+++ event_oop.py
@@ -9,6 +9,8 @@ class Event:
         self.event = event
         self.users = []
         
+
+        
         
 
 class Events:
@@ -32,7 +34,28 @@ class Events:
         self.events[event_id].event = event_name
         return "event edited successfully"
     
-    def view_event(self):
-        pass
-    def delete_event(self):
-        pass
+    def view_event(self,event_id):
+        if not event_id in self.events:
+            return "invalid event id"
+        match_event = self.events[event_id]
+        return f"id: {match_event.id} \n event: {match_event.event} user: {match_event.user} ",
+    
+    def view_all_events(self):
+        if not self.events:
+            return "no event found"
+        results = []
+        for e in self.events:
+            results.append(f"id:{e.id} event : {e.event}  users: {e.users}")
+            return "\n".join(results)
+    
+    def delete_event(self,event_id):
+        if not event_id in self.events:
+            return "invalid event id"
+        del self.events[event_id]
+        return "Event deleted successfully"
+    
+    class Enroller:
+        def __init__(self,events,user):
+            self.events =  events
+    
+    
